4e890223a55a4d717bc048d08c3e00e3d36fb809
Chocohead <Chocohead@Aroma1997.org>
2019-07-23 01:58:34 +0100

    Begin fleshing out Loom + MC plugin
    The Loom plugin will handle Mixin and remapping
    The Minecraft plugin will handle directly Minecraft related stuff like merging the jars and providing the mappings to Loom
    The Minecraft and Yarn versions can be given directly to the plugin or specified as a program arg

--- src/main/java/com/chocohead/loom/JkPluginLoom.java
+++ src/main/java/com/chocohead/loom/JkPluginLoom.java
@@ -2,9 +2,20 @@ package com.chocohead.loom;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.util.Collections;
 
+import dev.jeka.core.api.depmanagement.JkDependency;
+import dev.jeka.core.api.depmanagement.JkDependencyResolver;
+import dev.jeka.core.api.depmanagement.JkDependencySet;
+import dev.jeka.core.api.depmanagement.JkResolveResult;
+import dev.jeka.core.api.depmanagement.JkScopedDependency;
+import dev.jeka.core.api.system.JkLocator;
+import dev.jeka.core.api.system.JkLog;
 import dev.jeka.core.api.utils.JkUtilsIO;
+import dev.jeka.core.api.utils.JkUtilsPath;
 import dev.jeka.core.tool.JkCommands;
+import dev.jeka.core.tool.JkConstants;
 import dev.jeka.core.tool.JkDoc;
 import dev.jeka.core.tool.JkDocPluginDeps;
 import dev.jeka.core.tool.JkPlugin;
@@ -12,11 +23,36 @@ import dev.jeka.core.tool.builtins.java.JkPluginJava;
 
 @JkDocPluginDeps(JkPluginJava.class)
 public class JkPluginLoom extends JkPlugin {
+	/** Cache for persistently storing anything related to the project */
+	public final Path globalCache, projectCache;
+
+	protected final JkDependencyResolver depResolver = getCommands().getRunDependencyResolver();
+
 	@JkDoc("The url to display content.")
 	public String url = "https://www.google.com/";
 
 	protected JkPluginLoom(JkCommands commands) {
 		super(commands);
+
+		globalCache = JkLocator.getJekaUserHomeDir().resolve("cache/fabric-loom");
+		JkUtilsPath.createDirectories(globalCache);
+
+		projectCache = commands.getBaseDir().resolve(JkConstants.JEKA_DIR).resolve("fabric-loom");
+		JkUtilsPath.createDirectories(projectCache);
+	}
+
+	@Override
+	protected void init() {
+		JkLog.info("Loom init");
+	}
+
+	@Override
+	protected void activate() {
+		JkLog.info("Loom activate");
+	}
+
+	public JkResolveResult resolveDependency(JkDependency dependency) {
+		return depResolver.resolve(JkDependencySet.of(Collections.singletonList(JkScopedDependency.of(dependency)))).assertNoError();
 	}
 
 	@JkDoc("Display source cotent of the url option on the console.")
---
+++ src/main/java/com/chocohead/loom/minecraft/JkPluginMinecraft.java
@@ -0,0 +1,56 @@
+package com.chocohead.loom.minecraft;
+
+import dev.jeka.core.api.depmanagement.JkDependency;
+import dev.jeka.core.api.depmanagement.JkModuleDependency;
+import dev.jeka.core.api.system.JkException;
+import dev.jeka.core.api.system.JkLog;
+import dev.jeka.core.tool.JkCommands;
+import dev.jeka.core.tool.JkDoc;
+import dev.jeka.core.tool.JkDocPluginDeps;
+import dev.jeka.core.tool.JkPlugin;
+
+import com.chocohead.loom.JkPluginLoom;
+
+@JkDoc("Adds Minecraft support to Loom")
+@JkDocPluginDeps(JkPluginLoom.class)
+public class JkPluginMinecraft extends JkPlugin {
+	protected final JkPluginLoom loom = getCommands().getPlugin(JkPluginLoom.class);
+
+	@JkDoc("The Minecraft version to use")
+	public String version;
+	@JkDoc("The mappings to use")
+	public String yarnModule;
+
+	public JkDependency yarn;
+
+	protected JkPluginMinecraft(JkCommands commands) {
+		super(commands);
+	}
+
+	@Override
+	protected void init() {
+		JkLog.info("Minecraft init");
+	}
+
+	@Override
+	protected void activate() {
+		JkLog.info("Minecraft activate");
+
+		//We need a Minecraft version to start with
+		if (version == null) throw new JkException("Minecraft version not set");
+
+		//If the Yarn dependency isn't directly set, check for the module string
+		if (yarn == null) {
+			if (yarnModule == null) {
+				throw new JkException("Yarn version is not set for " + version);
+			} else {
+				JkLog.trace("Implying Yarn dependency from provided module: " + yarnModule);
+				yarn = JkModuleDependency.of(yarnModule);
+			}
+		} else {
+			JkLog.trace("Using supplied Yarn dependency: " + yarn);
+		}
+
+
+	}
+}
