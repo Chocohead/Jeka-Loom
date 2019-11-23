c976abf1ad3c55573e298861c6d8f7daf0e4c656
Chocohead <Chocohead@Aroma1997.org>
2019-11-23 04:11:17 +0000

    Move launch testing into testing package

--- src/test/java/com/chocohead/loom/LaunchTesting.java
+++ src/test/java/com/chocohead/loom/LaunchTesting.java
@@ -1,14 +1,16 @@
+package com.chocohead.loom;
+
 import dev.jeka.core.api.depmanagement.JkDependency;
+import dev.jeka.core.tool.JkCommands;
 import dev.jeka.core.tool.JkInit;
 
-import com.chocohead.loom.FullDependencies;
 import com.chocohead.loom.minecraft.EnigmaLiveDependency;
 import com.chocohead.loom.minecraft.JkPluginMinecraft;
 
 /** Rough example for how things can be interacted with, designed for debugging */
-public class Testing {
+public class LaunchTesting extends JkCommands {
 	public static void main(String[] args) {
-		JkPluginMinecraft mcPlugin = JkInit.instanceOf(Build.class, args).getPlugin(JkPluginMinecraft.class);
+		JkPluginMinecraft mcPlugin = JkInit.instanceOf(LaunchTesting.class, args).getPlugin(JkPluginMinecraft.class);
 
 		JkDependency mappings = EnigmaLiveDependency.createForBranch("Blayyke/yarn", "1.2.5");
 		FullDependencies minecraftDep = mcPlugin.standard("1.2.5").withMappings(mappings).splitMerge(true).build().asDependency();
